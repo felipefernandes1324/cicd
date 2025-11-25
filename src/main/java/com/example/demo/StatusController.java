5eb59fada9ee3e852f63fff8159452bad8003f96
felipefernandes1324 <felipefernandes1324@gmail.com>
2025-11-25 20:56:33 -0300

    Ajuste final

---
+++ src/main/java/com/example/demo/StatusController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @GetMapping("/status")
+    public ResponseEntity<Void> getStatus() {
+        return ResponseEntity.ok().build();
+    }
+}
